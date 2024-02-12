20f7b0fa965ab85a46930565b8c6588044f71215
Mukesh Mahato <mukeshmahato843@gmail.com>
2024-02-12 23:54:17 +0545

    Pages Added: Home, About, Contact, Service

--- homeservice/views.py
+++ homeservice/views.py
@@ -3,4 +3,16 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def service(request):
+    return render(request, "services.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
